131d17cbd20c62ab7b8c22fa84746c12d2be7c8e
TicianoFilho <ticianofilho@gmail.com>
2022-12-06 16:16:46 -0300

    feat: add DateConfig configuration class for whole app dateTime display format

---
+++ src/main/java/com/api/parkingspot/config/DateConfig.java
@@ -0,0 +1,26 @@
+package com.api.parkingspot.config;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+public class DateConfig {
+
+	private static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+	private static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+	
+	@Bean
+	@Primary
+	ObjectMapper objeckMapper() {
+		JavaTimeModule module = new JavaTimeModule();
+		module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+		return new ObjectMapper().registerModule(module);
+	}
+}
