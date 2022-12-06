69061289d712c9feb6d74a57eca6d57cb9ebf3e4
TicianoFilho <ticianofilho@gmail.com>
2022-12-06 17:54:41 -0300

    other: add Bean passwordEncoder class

--- src/main/java/com/api/parkingspot/config/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingspot/config/security/WebSecurityConfig.java
@@ -1,13 +1,21 @@
 package com.api.parkingspot.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.
@@ -23,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("ticiano")
-			.password("abc123")
+			.password(passwordEncoder().encode("abc123")) // the password must be encoded to Spring Security may read it. 
 			.roles("ADMIN");
 			
 	}
