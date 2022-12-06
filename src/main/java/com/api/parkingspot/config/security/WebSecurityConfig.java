6794227190283914e74d6cfa5ceb73aaecf5dcfa
TicianoFilho <ticianofilho@gmail.com>
2022-12-06 17:43:41 -0300

    other: add configure method for user authentication

--- src/main/java/com/api/parkingspot/config/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingspot/config/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.api.parkingspot.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,5 +18,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.csrf().disable();
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("ticiano")
+			.password("abc123")
+			.roles("ADMIN");
+			
+	}
+	
+	
 
 }
