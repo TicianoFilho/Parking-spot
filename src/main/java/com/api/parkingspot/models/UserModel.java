e2a6e22f69f0839afd1c3a377733b081f15633b4
TicianoFilho <ticianofilho@gmail.com>
2022-12-07 14:39:44 -0300

    feat: add userModel class

---
+++ src/main/java/com/api/parkingspot/models/UserModel.java
@@ -0,0 +1,28 @@
+package com.api.parkingspot.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class UserModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+
+}
