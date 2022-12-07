185006ae23bb3027721c05653ef3b3db337e50fe
TicianoFilho <ticianofilho@gmail.com>
2022-12-07 17:04:53 -0300

    feat: add role class

---
+++ src/main/java/com/api/parkingspot/models/RoleModel.java
@@ -0,0 +1,56 @@
+package com.api.parkingspot.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.api.parkingspot.enums.RoleName;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class RoleModel implements Serializable, GrantedAuthority {
+	private static long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, unique = true)
+	private RoleName roleName;
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return this.roleName.toString();
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public RoleName getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(RoleName roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
+
